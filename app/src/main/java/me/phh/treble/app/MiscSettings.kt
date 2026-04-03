55873b1017a1c4f0d11cfdb3607674363d6e1cd3
Doze-off <viniciussadxxx@gmail.com>
2026-04-03 20:43:14 -0300

    Update MiscSettings.kt

--- app/src/main/java/me/phh/treble/app/MiscSettings.kt
+++ app/src/main/java/me/phh/treble/app/MiscSettings.kt
@@ -25,6 +25,7 @@ object MiscSettings : Settings {
     val dt2w = "key_misc_dt2w"
     val activityAnimPerfOverride = "key_misc_activity_anim_perf_override"
     val lmkTweaks = "key_misc_lmk_tweaks"
+    val preferSwCodecs = "key_misc_prefer_sw_codecs"
 
 
 
