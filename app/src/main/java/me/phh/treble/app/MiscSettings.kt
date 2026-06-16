859d6ca5630806d1c7285e3f44ac9746a7dcb116
Doze-off <viniciussadxxx@gmail.com>
2026-06-16 12:27:42 -0300

    Add preferSwCodecs key to MiscSettings
    By @cawilliamson

--- app/src/main/java/me/phh/treble/app/MiscSettings.kt
+++ app/src/main/java/me/phh/treble/app/MiscSettings.kt
@@ -26,6 +26,7 @@ object MiscSettings : Settings {
     val activityAnimPerfOverride = "key_misc_activity_anim_perf_override"
     val lmkTweaks = "key_misc_lmk_tweaks"
     val preferHwCodecs = "key_misc_prefer_hw_codecs"
+    val preferSwCodecs = "key_misc_prefer_sw_codecs"
     val bluetoothFix =  "key_misc_bluetooth_fix"
     val hostspotfix =  "key_misc_hostspot_fix"
     
